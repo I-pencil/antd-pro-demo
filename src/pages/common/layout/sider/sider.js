4e7ab98e49a489e7cb2ffd5e5b9c8621dda4f966
lili_sakura@126.com <lili_sakura@126.com>
2018-10-22 23:22:56 +0800

    Layout Header

--- src/pages/common/layout/sider/sider.js
+++ src/pages/common/layout/sider/sider.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 import isEqual from 'lodash/isEqual';
 import pathToRegexp from 'path-to-regexp';
 import styles from './sider.less';
@@ -10,10 +10,9 @@ const { Sider } = Layout;
 const MenuItem = Menu.Item;
 const SubMenu = Menu.SubMenu;
 
-export default class SiderWrapper extends React.Component {
+export default class SiderContainer extends React.Component {
   state = {
     openKeys: [],
-    inlineCollapsed: false,
     selectedKeys: [],
   };
   componentDidMount() {
@@ -40,7 +39,7 @@ export default class SiderWrapper extends React.Component {
           );
         });
       }
-      return <SubMenu key={submenu.id} title={submenu.name}>
+      return <SubMenu key={submenu.id} title={<span>{submenu.icon && <Icon type={submenu.icon} />}<span>{submenu.name}</span></span>}>
         {getMenuItems()}
       </SubMenu>
     })
@@ -55,7 +54,7 @@ export default class SiderWrapper extends React.Component {
       }
     });
     if (selectedItem) {
-      const openKeys = menus.filter((item) => item.pid === selectedItem.pid).map(item => Object.prototype.toString(item.id));
+      const openKeys = menus.filter((item) => item.id === selectedItem.pid).map(item => String(item.id));
       this.setState({
         openKeys,
         selectedKeys: [`${selectedItem.id}`]
@@ -64,50 +63,23 @@ export default class SiderWrapper extends React.Component {
   }
 
   render() {
-    const { inlineCollapsed, openKeys, selectedKeys } = this.state;
+    const { openKeys, selectedKeys } = this.state;
+    const { collapsed } = this.props;
+    console.log('this.props.collapsed', collapsed);
+
     const menuProps = {
-      openKeys,
-      selectedKeys,
+      inlineCollapsed: collapsed,
     }
     return (
       <div>
         <Sider className={styles.container}>
-          <div className={styles.logo}><h1>Pro Demo</h1></div>
+          <div className={styles.logo} style={{ display: collapsed ? 'none': 'block' }}><h1>Pro Demo</h1></div>
           <Menu
             mode="inline"
             theme="dark"
-            inlineCollapsed={inlineCollapsed}
             {...menuProps}
           >
             {this.getMenus()}
-            <SubMenu title="Dashboard">
-              <MenuItem><Link to='/dashboard/analysis'>分析页</Link></MenuItem>
-              <MenuItem><Link to='/dashboard/monitor'>监控页</Link></MenuItem>
-              <MenuItem><Link to='/dashboard/workplace'>工作台</Link></MenuItem>
-            </SubMenu>
-            <SubMenu title="表单页">
-              <MenuItem><Link to=''>基础表单</Link></MenuItem>
-              <MenuItem><Link to=''>分步表单</Link></MenuItem>
-              <MenuItem><Link to=''>高级表单</Link></MenuItem>
-            </SubMenu>
-            <SubMenu title="列表页">
-              <MenuItem><Link to=''>查询表格</Link></MenuItem>
-              <MenuItem><Link to=''>标准列表</Link></MenuItem>
-              <MenuItem><Link to=''>卡片列表</Link></MenuItem>
-              <MenuItem><Link to=''>搜索列表</Link></MenuItem>
-            </SubMenu>
-            <SubMenu title="详情页">
-              <MenuItem><Link to=''>基础详情页</Link></MenuItem>
-              <MenuItem><Link to=''>高级详情页</Link></MenuItem>
-            </SubMenu>
-            <SubMenu title="结果页">
-              <MenuItem><Link to=''>成功页</Link></MenuItem>
-              <MenuItem><Link to=''>失败页</Link></MenuItem>
-            </SubMenu>
-            <SubMenu title="个人页">
-              <MenuItem><Link to=''>个人中心</Link></MenuItem>
-              <MenuItem><Link to=''>个人设置</Link></MenuItem>
-            </SubMenu>
           </Menu>
         </Sider>
       </div>
